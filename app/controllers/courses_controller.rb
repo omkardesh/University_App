317bed234672fceaf398da13a38428f35f779189
Omkar Deshpande <omkard199@gmail.com>
2019-09-26 11:22:37 +0530

    Added navbar and sticky footer

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,4 +2,8 @@ class CoursesController < ApplicationController
   def index
 
   end
+
+  def new
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
   root 'courses#index'
 
+  get 'courses/new', to: 'courses#new'
+  get 'about', to: 'pages#about'
+
 end
